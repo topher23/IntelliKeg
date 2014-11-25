7e3124b520784798bfd115b7c7706a56316a9b76
Christopher Dorick <topher23@vt.edu>
2014-11-25 04:50:40 +0000

    add logic to flowmeter, pricing works (for now), and initial commit for solenoid.py.

--- flowmeter.py
+++ flowmeter.py
@@ -1,24 +1,40 @@
 
 import time
+import datetime
 import RPi.GPIO as GPIO
 
 
 GPIO.setmode(GPIO.BOARD)
-count = 0
 GPIO.setup(12, GPIO.IN)
 
 
-while True:
-	prevtest = GPIO.input(12)
-	time.sleep(.01)
-	test = GPIO.input(12)
 
-	if test == 0 and prevtest == 1:
-		count += 1
-	elif test == 1 and prevtest == 0:
-		count += 1
-	
-	print prevtest
-	print test
-	print count
-#	time.sleep(.1)
+
+def flowing (price):
+	count = 0
+	realtime = time.time()
+	prevtime = time.time()
+
+	while True:
+		prevtest = GPIO.input(12)
+		time.sleep(.01)
+		test = GPIO.input(12)
+
+		if (test == 0 and prevtest == 1) or (test == 1 and prevtest == 0):
+			count += 1
+			prevtime = time.time()
+			realtime = time.time()
+		else:
+			realtime = time.time()
+
+		tdelta = realtime - prevtime
+		if tdelta > 2:
+			break
+
+	return count
+
+
+
+
+
+flowing (1)
---
+++ pricing.py
@@ -0,0 +1,16 @@
+
+
+
+
+
+def pricing(cost, amount):
+	toCharge = cost * amount
+	return toCharge
+
+
+
+
+
+
+
+pricing (.00571, 350)
